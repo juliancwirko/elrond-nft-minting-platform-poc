44cc3042946ab01f9c9fcae8304d4cd2bc6eb114
Julian Ćwirko <julian.cwirko@gmail.com>
2022-02-19 13:28:33 +0100

    new vid

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,28 +12,48 @@ const Home: React.FC = () => {
   return (
     <MainLayout>
       <Pane backgroundColor="white" elevation={1} padding={30}>
-        <Paragraph marginBottom={30}>
-          This app is a result of learning and playing with{' '}
-          <Link href="https://elrond.com/">Elrond</Link> blockchain and its{' '}
-          <Link href="https://github.com/ElrondNetwork">tooling</Link>.
-        </Paragraph>
+        <Pane display="flex" alignItems="center" justifyContent="space-between" marginBottom={40}>
+          <Paragraph marginBottom={30}>
+            This app is a result of learning and playing with{' '}
+            <Link href="https://elrond.com/">Elrond</Link> blockchain and its{' '}
+            <Link href="https://github.com/ElrondNetwork">tooling</Link>.
+          </Paragraph>
 
-        <Paragraph marginBottom={30}>
-          <Button onClick={unlock}>Unlock Elven Tools</Button>
-          <Link marginLeft={10} href="https://github.com/juliancwirko">
-            Github
-          </Link>
-        </Paragraph>
+          <Paragraph marginBottom={30}>
+            <Link
+              href="https://github.com/juliancwirko/elrond-nft-minting-platform-poc"
+            >
+              Github
+            </Link>
+            <Button marginLeft={20} onClick={unlock}>Unlock Elven Tools</Button>
+          </Paragraph>
+        </Pane>
 
-        <Paragraph marginBottom={10}>Updates:</Paragraph>
         <Paragraph>
-          <Heading marginBottom={10}>elven-tools-cli sneak peek:</Heading>{' '}
+          <Heading
+            marginBottom={10}
+            textAlign="center"
+            maxWidth={800}
+            marginLeft="auto"
+            marginRight="auto"
+            width="100%"
+          >
+            'How to run NFT collection on the Elrond blockchain' - from
+            preparing the assets through Smart Contract deployment to buying and
+            minting with the CLI tool.
+          </Heading>{' '}
           <Pane marginBottom={30}>
-            <Pane maxWidth={560} width="100%">
+            <Pane
+              maxWidth={800}
+              marginTop={40}
+              marginLeft="auto"
+              marginRight="auto"
+              width="100%"
+            >
               <iframe
                 width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/VKVy4Bx-WVQ"
+                height="450"
+                src="https://www.youtube.com/embed/resGP6a7_34"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -41,6 +61,19 @@ const Home: React.FC = () => {
               ></iframe>
             </Pane>
           </Pane>
+        </Paragraph>
+        <Paragraph marginTop={40}>
+          <Heading marginBottom={10}>More articles and tools:</Heading>
+        </Paragraph>
+        <Paragraph>
+          <Link href="https://www.elven.tools/docs/tips-on-buying-nfts-on-the-elrond-blockchain.html">
+            - Tips on buying NFTs on the Elrond blockchain
+          </Link>
+        </Paragraph>
+        <Paragraph>
+          <Link href="https://www.elven.tools/docs/elven-tools-workflows.html">
+            - Elven Tools Workflows
+          </Link>
         </Paragraph>
         <Paragraph>
           <Link href="https://www.julian.io/articles/elrond-erdjs.html">
@@ -78,44 +111,11 @@ const Home: React.FC = () => {
           <Link href="https://github.com/juliancwirko/elven-mint">
             elven-mint
           </Link>{' '}
-          and.
         </Paragraph>
 
-        <Paragraph marginBottom={30}>
-          Elven Tools is a replication of NFTs creation functionality only on
-          the devnet. Using it, you can issue ESDT tokens for NFTs, create NFTs,
-          displaying already created NFTs and transactions. All done using React
-          tools from Elrond.
-        </Paragraph>
-
-        <Paragraph marginBottom={30}>
-          You would need to have the devnet address. You can use a couple of
-          different ways to unlock and sign transactions. For now, it was tested
-          using web wallet and Maiar browser extension.
-        </Paragraph>
-
-        <Heading>Tooling:</Heading>
-        <Paragraph>
-          1. Elrond tooling. Mainly{' '}
-          <Link href="https://www.npmjs.com/package/@elrondnetwork/dapp">
-            React tools
-          </Link>
-          , and{' '}
-          <Link href="https://www.npmjs.com/package/@elrondnetwork/erdjs">
-            JavaScript SDK
-          </Link>
-          .
-        </Paragraph>
-        <Paragraph marginBottom={30}>
-          2. IPFS based tooling - The app takes any URL for the asset. The best
-          is to use <Link href="https://nft.storage/">NFT.storage</Link> free,
-          decentralized storage, and bandwidth for NFTs on{' '}
-          <Link href="https://ipfs.io/">IPFS</Link> and{' '}
-          <Link href="https://filecoin.io/">Filecoin</Link>. It should provide a
-          pinning service as well.
-        </Paragraph>
-
-        <Heading>What you need to play with it:</Heading>
+        <Heading>
+          What you need to play with the website on which you are:
+        </Heading>
         <Paragraph>
           1. Create your wallet address using the devnet. You can do this here:{' '}
           <Link href="https://devnet-wallet.elrond.com/">
@@ -140,24 +140,6 @@ const Home: React.FC = () => {
           5. Then you can start creating NFTs.
         </Paragraph>
 
-        <Heading>Roadmap (for the devnet POC ;)):</Heading>
-        <Paragraph>- Proper Smart Contract implementation</Paragraph>
-        <Paragraph>
-          - Possibility to mint single NFTs and Collection using a minter Smart
-          Contract (probably separate tools)
-        </Paragraph>
-        <Paragraph>- Possibility to list it and sell</Paragraph>
-        <Paragraph>
-          - Possibility to list a collection - minted using other tooling, for
-          example elrond-tools-cli
-        </Paragraph>
-        <Paragraph>- Much better UI/UX</Paragraph>
-        <Paragraph>- Other improvements</Paragraph>
-        <Paragraph marginBottom={30}>
-          - If it will make sense, and if it will work well - deploy to the
-          mainnet (??? probably not, to much devops work is required ;))
-        </Paragraph>
-
         <Heading>If you want to contact me:</Heading>
         <Paragraph>julian.cwirko@gmail.com</Paragraph>
         <Paragraph>
@@ -171,32 +153,11 @@ const Home: React.FC = () => {
           </Link>
         </Paragraph>
 
-        <Heading size={500} marginBottom={30}>
-          Until I figure out and learn a couple of more things, there are no
-          plans to make it a thing, but maybe it will become usable during my
-          learning process. Maybe someone could take something from that. It
-          will probably shift more towards{' '}
-          <Link href="https://github.com/juliancwirko/elven-tools-cli">
-            elven-tools-cli
-          </Link>{' '}
-          + maybe the devnet marketplace POC later.
-        </Heading>
-
         <Heading>
           Also, check out my other learning resources and tools regarding Elrond
           blockchain:
         </Heading>
 
-        <Paragraph>
-          <Link href="https://github.com/juliancwirko/elven-mint">
-            - elven-mint tool
-          </Link>
-        </Paragraph>
-        <Paragraph>
-          <Link href="https://github.com/juliancwirko/nft-art-maker">
-            - nft-art-maker tool
-          </Link>
-        </Paragraph>
         <Paragraph>
           <Link href="https://www.julian.io/articles/elrond-smart-contracts.html">
             - Smart Contract for Elrond and Arwen
